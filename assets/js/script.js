45849e45b33701aac314c04d0cbc23555bc777b1
LipeFelps <lipefelps2002@gmail.com>
2021-10-07 19:15:25 -0300

    web fundamentos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,71 @@
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
-/*
-Case Sensitive = Reconhece letras maiusculas e minusculas
-
-Posso acessar por:
-Tag: getElementByTagName()
-Id: getElementById()
-Nome: getElemntByName()
-Classe: getElementsByClassName()
-Seletor: querySelector()
-*/
-let nome = window.document.getElementById('nome')
-let email = document.querySelector('#email')
-let assunto = document.querySelector('#assunto')
-
-nome.style.widht = '100%'
-email.style.widht = '100%'
+nome.style.width = "100%"
+email.style.width = "100%"
+assunto.style.width = "100%"
 
 function validaNome() {
-    let txtNome = document.querySelector('#txtNome')
-    if (nome.value.lenght < 3) {
-        txtNome.innerHTML = 'Nome Ivalido'
-        txtNome.style.color = 'red'
-  } else {
-      txtNome.innerHTML = 'Nome Valido'
-      txtNome.style.color = 'green'
-  }
 
-}
-
-function validaEmail() {
-    let txtEmail = document.querySelector('#email')   
-    if (email.value.indexOf('@')== -1 || email.value.indexOf('.'))   {
-    txtEmail.innerHTML = 'E-mail Invalido '
-    txtEmail.style.color = 'red'
-} else {
-
-    txtEmail.innerHTML = 'E-mail valido'
-    txtEmail.style.color = 'green'
-}
-}
-
-function validaAssunto() {
-    let txtAssunto = document.querySelector('#txtassunto')
-
-    if (assunto.value.lenght >= 100) {
-    txtAssunto.innerHTML = 'Texto muito grande, maximo de 100 caracteres'
-    txtAssunto.style.color = 'red'
-    txtAssunto.style.display
-
+    let txtNome = document.querySelector("#txtNome")
+    if (nome.value.length < 3) {
+        
+        txtNome.innerHTML = "Nome inválido!"
+        txtNome.style.color = "red"
+    } else {
+        txtNome.innerHTML = "Nome válido!"
+        txtNome.style.color = "green"
+        nomeOk = true
     }
 }
 
+function validaEmail() {
+    let txtEmail = document.querySelector("#txtEmail")
 
+    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+
+    txtEmail.innerHTML = "E-mail inválido!"
+    txtEmail.style.color = "red"
+    } else {
+        txtEmail.innerHTML = "E-mail válido!"
+        txtEmail.style.color = "green"
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if (assunto.value.length > 100) {
+        txtAssunto.innerHTML = "Texto muito grande. Digite o máximo até 100 caractéres!"
+        txtAssunto.style.color = "red"
+        txtAssunto.style.display = "block"
+    } else {
+        txtAssunto.style.display = "none"
+        assuntoOk = true
+    }
+}
+
+function enviar () {
+
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert("Formulário enviado com sucesso!")
+    } else {
+        alert("Preencha o formulário corretamente antes de enviar...")
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = "650px"
+    mapa.style.height = "600px"
+}
+
+function mapaNormal() {
+    mapa.style.width = "350px"
+    mapa.style.height = "300px"
+}
